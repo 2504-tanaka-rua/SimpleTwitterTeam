69837fda18f21a02d5d672582a3603fd03ecc95f
yokosuka_susumu <yokosuka.susumu@alhinc.jp>
2025-06-03 15:42:16 +0900

    test2

--- src/chapter6/controller/TopServlet.java
+++ src/chapter6/controller/TopServlet.java
@@ -37,7 +37,7 @@ public class TopServlet extends HttpServlet {
 		String end = request.getParameter("end");
 		List<UserMessage> messages = new MessageService().select(userId, start, end, searchWord, radiobutton);
 		request.setAttribute("likeSearch", 1);
-		// コンフリクトです
+		String test = "コンフリクトでうｓ";
 
 		if (!StringUtils.isBlank(radiobutton) && radiobutton.equals("contain")) {
 			request.setAttribute("likeSearch", 2);
